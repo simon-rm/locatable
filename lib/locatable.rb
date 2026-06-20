2ccc9e82ae6268fbd45be65f9a0f814ef3b9fcde
simon <simonrmurcia@gmail.com>
2026-06-20 15:15:59 +0000

    add activerecord-postgis-adapter

--- lib/locatable.rb
+++ lib/locatable.rb
@@ -1,6 +1,8 @@
 require_relative "locatable/version"
 require_relative "locatable/migration_helpers"
 
+require "activerecord-postgis-adapter"
+
 require "locatable/railtie" if defined?(Rails::Railtie)
 
 module Locatable
